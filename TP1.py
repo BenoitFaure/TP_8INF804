ae03e44183f6bd5da0f7b50e909c803582678c83
BenoitFaure <benoit.faure.cesar@gmail.com>
2023-10-05 16:45:14 -0400

    checkpoint - still a mess

--- TP1.py
+++ TP1.py
@@ -37,42 +37,113 @@ def load_other_images(folder: str) -> List[np.array]:
 
 # ------- DEFINE TECHNIQUE -------
 
+def extr_bb(img_c, image):
+
+    # Find contours in the thresholded image
+    contours, _ = cv2.findContours(img_c, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Make bounding boxes
+    bbs = [[*cv2.boundingRect(contour)] for contour in contours]
+
+    # Get area and of bounding boxes
+    areas = [bb[2] * bb[3] for bb in bbs]
+
+    # Numpy arrays for ease
+    bbs = np.array(bbs)
+    areas = np.array(areas)
+    
+    # Check if bbs
+    if len(bbs.shape) < 2:
+        return image
+
+    # Switch to x, y, xp, yp
+    bbs[:, 2] = bbs[:, 0] + bbs[:, 2]
+    bbs[:, 3] = bbs[:, 1] + bbs[:, 3]
+
+    # Filter by size
+    area_th = 5000
+    mask_area = areas > area_th
+
+    bbs = bbs[mask_area, :]
+    areas = areas[mask_area]
+
+    # Combine by overlap
+    def combine_bbs(sim_bbs):
+
+        x = sim_bbs[:, 0].min()
+        y = sim_bbs[:, 1].min()
+        xp = sim_bbs[:, 2].max()
+        yp = sim_bbs[:, 3].max()
+
+        return [x, y, xp, yp]
+
+    def overlap(a, b):
+
+        intersection = max(0, min(a[2], b[2]) - max(a[0], b[0])) * max(0, min(a[3], b[3]) - max(a[1], b[1]))
+
+        area_a = (a[2] - a[0]) * (a[3] - a[1])
+        area_b = (b[2] - b[0]) * (b[3] - b[1])
+
+        return intersection / min(area_a, area_b)
+
+
+    def get_all_overlap(bb, others):
+
+        combine = []
+        exclude = []
+        for b in others:
+            
+            if overlap(bb, b) > 0.1:
+                combine.append(b)
+            else:
+                exclude.append(b)
+        
+        return combine, exclude
+    
+    bbs = bbs.tolist()
+    clean_run = False
+    while not clean_run:
+        clean_run = True
+        new_bbs = []
+        while len(bbs) > 0:
+
+            bb = bbs.pop(0)
+            combine, bbs = get_all_overlap(bb, bbs)
+
+            if len(combine) > 0:
+                clean_run = False
+
+            combine.append(bb)
+            new = combine_bbs(np.array(combine))
+            new_bbs.append(new)
+        
+        bbs = new_bbs
+
+    # Iterate through the detected contours and draw bounding boxes
+    for bb in bbs:
+        x, y, xp, yp = bb
+        cv2.rectangle(image, (x, y), (xp, yp), (0, 255, 0), 5)
+    
+    return image
+
 def make_bb(image, reference, mask):
 
-    # Map the reference luminosity to the image
-    img_lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-    ref_lab = cv2.cvtColor(reference, cv2.COLOR_BGR2LAB)
+    # Image to grayscale
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) * mask
+    ref = cv2.cvtColor(reference, cv2.COLOR_BGR2GRAY) * mask
 
-    # img_h = cv2.calcHist([img_lab[:, :, 0]], [0], None, [256], [0, 256])
-    # ref_h = cv2.calcHist([ref_lab[:, :, 0]], [0], None, [256], [0, 256])
-
-    # img_cdf = img_h.cumsum()
-    # ref_cdf = ref_h.cumsum()
-
-    # img_cdf = img_cdf / img_cdf.max()
-    # ref_cdf = ref_cdf / ref_cdf.max()
-
-    # mapping = np.interp(img_cdf, ref_cdf, np.arange(256))
-
-    # img_lab[:, :, 0] = ref_lab[:, :, 0] # mapping[img_lab[:, :, 0]]
-
-    img = cv2.cvtColor(img_lab, cv2.COLOR_LAB2BGR)
-    ref = cv2.cvtColor(ref_lab, cv2.COLOR_LAB2BGR)
-
-    a = img_lab[:, :, 0]
-    b = ref_lab[:, :, 0]
-
-    a = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    b = cv2.cvtColor(reference, cv2.COLOR_BGR2GRAY)
-
+    # Find contours
     kernel = np.ones((100, 100), np.float32) / 10000
-    b = cv2.absdiff(b, cv2.filter2D(b, -1, kernel))
-    a = cv2.absdiff(a, cv2.filter2D(a, -1, kernel))
+    img_c = cv2.absdiff(img, cv2.filter2D(img, -1, kernel))
+    ref_c = cv2.absdiff(ref, cv2.filter2D(ref, -1, kernel))
 
     # Calculate diff
-    diff_image = cv2.absdiff(a, b)
+    diff_c = cv2.absdiff(img_c, ref_c)
 
-    return a, b, diff_image #cv2.cvtColor(diff_image, cv2.COLOR_BGR2GRAY)
+    # Threshold
+    _, thr_c = cv2.threshold(diff_c, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    return diff_c, thr_c, extr_bb(diff_c, image)
 
 def make_bb2(image, reference, mask):
 
